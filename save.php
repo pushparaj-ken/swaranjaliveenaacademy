674ac7d791047ce41f5f6d40d2e4e4203a7c88c3
pushparaj-ken <policepushparaj96@gmail.com>
2024-04-05 18:36:33 +0530

    changes completed

--- save.php
+++ save.php
@@ -1,8 +1,6 @@
 <?php
 //print_r("echo");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
 // $name = $_POST['name'];
 // $email = $_POST['email'];
