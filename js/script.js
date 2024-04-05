67abff1ed6599dd3c74aa4db7224234e18078607
pushparaj-ken <policepushparaj96@gmail.com>
2024-04-05 17:35:53 +0530

    chnages completed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 
-$('.contact-form').submit(function (e) {
+$('body').on("submit", '.contact-form', function (e) {
   e.preventDefault();
-  $.ajax({
-    type: 'POST',
-    url: "save.php",
-    data: new FormData(this),
-    dataType: "json",
-    contentType: false,
-    processData: false,
-    success: function (response) {
-      if (response.result == 1) {
-        toastr.success('Thanks for reaching us ! We will contact you soon');
-        setTimeout(function () {
-          location.reload();
-        }, 1000);
-      } else {
-        toastr.error('Something went wrong! Please try again later!');
+  debugger
+  var this_id = 'form[this_id=' + $(this).attr('this_id') + ']';
+
+  if (is_required(this_id) === true) {
+    $.ajax({
+      type: 'POST',
+      url: "save.php",
+      data: new FormData(this),
+      dataType: "json",
+      contentType: false,
+      processData: false,
+      beforeSend: function () {
+        $(this_id + ' button[type=submit]').attr('disabled', 'true');
+      },
+      success: function (response) {
+        console.log(response)
+        if (response.result == 1) {
+          $(this_id)[0].reset();
+          alert('Success!');
+          $(this_id + ' button[type=submit]').removeAttr('disabled');
+
+          if ($(this_id).attr('reload-action') === 'true') {
+            setTimeout(function () { location.reload(); }, 1000);
+          }
+        } else {
+          alert('Something went wrong! Please try again later!');
+          $(this_id + ' input[type=submit]').removeAttr('disabled');
+          $(this_id + ' button[type=submit]').removeAttr('disabled');
+        }
       }
-    }
-  });
+    });
+  } else {
+    alert('Please check the required fields!');
+  }
 
 });
 
@@ -32,4 +48,30 @@ document.onreadystatechange = function () {
       document.getElementById('loader').style.visibility = "hidden";
     }, 150000);
   }
+}
+
+function is_required(this_id) {
+  $('.error-span').hide();
+  var inc = 0;
+  $(this_id + " .required").each(function () {
+    console.log($(this).attr('name'));
+    if ($(this).val() !== "undefined") {
+      if ($(this).val() != null) {
+        if (($(this).val()).length > 0) {
+
+        } else {
+          console.log($(this).attr('name'));
+          $(this).next("span").show();
+          inc++;
+        }
+      } else {
+        alert('Something went wrong on ' + $(this).attr('name'));
+        inc++;
+      }
+    }
+  });
+  if (inc === 0) {
+    return true;
+  }
+  return false;
 }
