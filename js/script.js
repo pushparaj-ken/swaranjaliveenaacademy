d0213e537ed54a1cd38cfcc20c5098717e953834
pushparaj-ken <policepushparaj96@gmail.com>
2024-04-05 16:33:16 +0530

    chnages completed

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$('.contact-form').submit(function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: 'GET',
+    url: "save.php",
+    data: new FormData(this),
+    dataType: "json",
+    contentType: false,
+    processData: false,
+    success: function (response) {
+      if (response.result == 1) {
+        toastr.success('Thanks for reaching us ! We will contact you soon');
+        setTimeout(function () {
+          location.reload();
+        }, 1000);
+      } else {
+        toastr.error('Something went wrong! Please try again later!');
+      }
+    }
+  });
+
+});
+
+
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'complete') {
+    setTimeout(function () {
+      document.getElementById('interactive');
+      document.getElementById('loader').style.visibility = "hidden";
+    }, 150000);
+  }
+}
---
+++ save.php
@@ -0,0 +1,31 @@
+<?php
+print_r("echo");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone_number = $_POST['phone_number'];
+$message = $_POST['message'];
+
+$from = "no-reply@kindercab.in";
+$to = "policepushparaj96@gmail.com";
+$subject = "Contact Form ";
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$headers .= "From: " . $from . "\r\n" .
+  "CC: " . $cc;
+
+$msg = "Gurdian Name:" . $_POST['gname'] . "<br>   Gurdian Phonenumber: " . $_POST['gphonenumber'] . "<br> Child Name: " . $_POST['cname'] . "<br>   Child Age: " . $_POST['cage'] . "<br> Message: " . $_POST['message'];
+
+// Send the first email
+if (mail($to, $subject, $msg, $headers)) {
+  $data['result'] = 1;
+} else {
+  $data['result'] = 0;
+}
+
+echo json_encode($data);
